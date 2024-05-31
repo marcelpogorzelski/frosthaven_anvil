7c8bd07f0e449ee9f16859d08582b8fe9b302af5
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2024-05-31 12:37:27 +0000

    Created Party.RowTemplate1; Edited Party

--- client_code/Party/RowTemplate1/__init__.py
+++ client_code/Party/RowTemplate1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import PartyTemplate
+from ._anvil_designer import RowTemplate1Template
 from anvil import *
 import anvil.server
 import anvil.users
@@ -7,10 +7,8 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Party(PartyTemplate):
+class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.repeating_panel_1.items = app_tables.characters.search()
-
     # Any code you write here will run before the form opens.
