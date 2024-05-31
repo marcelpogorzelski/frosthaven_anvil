89110d26c62d549c96955134bcea5c32564b2ece
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2024-05-31 18:14:28 +0000

    Edited Party; Deleted Party.RowTemplate1

--- client_code/Party/__init__.py
+++ client_code/Party/__init__.py
@@ -11,6 +11,17 @@ class Party(PartyTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.repeating_panel_1.items = app_tables.characters.search()
+    self.item = app_tables.characters.get(Player='Marcel')
+    #self.repeating_panel_1.items = app_tables.characters.search()
 
     # Any code you write here will run before the form opens.
+
+  def outlined_1_change(self, **event_args):
+    """This method is called when the text in this text box is edited"""
+    print(self.item['Name'])
+    print(event_args['sender'].text)
+    self.item.update()
+
+  def outlined_1_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
