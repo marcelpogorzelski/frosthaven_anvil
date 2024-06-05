f17252469f95f67b5df0c66d14fd825be21f33a0
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2024-06-05 09:11:31 +0000

    Created FrosthavenOld; Edited Character and Frosthaven; Deleted Resources.ItemTemplate1

--- client_code/FrosthavenOld/__init__.py
+++ client_code/FrosthavenOld/__init__.py
@@ -1,14 +1,14 @@
-from ._anvil_designer import FrosthavenTemplate
+from ._anvil_designer import FrosthavenOldTemplate
 from anvil import *
-import anvil.server
 import anvil.users
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 from .. import Utilites
 
 
-class Frosthaven(FrosthavenTemplate):
+class FrosthavenOld(FrosthavenOldTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
@@ -19,3 +19,4 @@ class Frosthaven(FrosthavenTemplate):
   def text_box_change(self, **event_args):
     """This method is called when the text in this text box is edited"""
     self.total_defense_text_box.text = Utilites.get_total_defense(moral=self.moral_text_box.text, defense=self.bonus_defense_text_box.text)
+
