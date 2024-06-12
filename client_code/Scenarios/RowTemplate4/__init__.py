35f3eb7bab7b2d475309b09742c59cdd77145cda
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2024-06-12 07:13:43 +0000

    Edited Scenarios.RowTemplate4 and Scenarios

--- client_code/Scenarios/RowTemplate4/__init__.py
+++ client_code/Scenarios/RowTemplate4/__init__.py
@@ -13,10 +13,7 @@ class RowTemplate4(RowTemplate4Template):
 
   def foregroud_color(self, hex):
     red, green, blue = tuple(int(hex[i:i+2], 16) for i in (1, 3, 5))
-    print(red, (red/255.0) ** 2))
-    #let cieY = Math.pow(sR/255.0,2.2) * 0.2126 +
-    #      Math.pow(sG/255.0,2.2) * 0.7152 +
-    #      Math.pow(sB/255.0,2.2) * 0.0722; 
+    
     cieY = (((red/255.0) ** 2) *  0.2126 ) + (((green/255.0) ** 2) *  0.7152) + (((blue/255.0) ** 2) *  0.0722)
     if cieY < 0.36:
       return "#ffffff"
