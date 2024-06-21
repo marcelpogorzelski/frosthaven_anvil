28bfaa721161040a3396afbf794bcc2526f70f16
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2024-06-21 04:51:53 +0000

    Edited Character, Frosthaven, ImportExport, Login and 1 more

--- client_code/Character/__init__.py
+++ client_code/Character/__init__.py
@@ -6,18 +6,30 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 from .. import Utilites
+from anvil.js.window import window
 
 
 class Character(CharacterTemplate):
   def __init__(self, player, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+
+    self.adjust_width()
+    
     self.player = player
     self.item = app_tables.characters.get(Player=self.player)
   
     self.level_text_box.text = Utilites.get_level(self.item['Experience'])
 
     self.populate_class_drop_down()
+
+  def adjust_width(self):
+    if window.innerWidth < 900:
+      column_width = (window.innerWidth / 3) - 10
+      data_grid_columns = self.data_grid_1.columns
+      for column in data_grid_columns:
+        column['width'] = column_width
+      self.data_grid_1.columns = data_grid_columns
     
   def populate_class_drop_down(self):
     item_list = []
--- client_code/Frosthaven/__init__.py
+++ client_code/Frosthaven/__init__.py
@@ -13,16 +13,21 @@ class Frosthaven(FrosthavenTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+
+    self.adjust_width()
+    
     self.item = app_tables.frosthaven.search()[0]
+    self.total_defense_text_box.text = Utilites.get_total_defense(
+      moral=self.item["Moral"], defense=self.item["Defense"]
+    )
+
+  def adjust_width(self):
     if window.innerWidth < 900:
       column_width = (window.innerWidth / 3) - 10
       data_grid_columns = self.data_grid_1.columns
       for column in data_grid_columns:
         column['width'] = column_width
       self.data_grid_1.columns = data_grid_columns
-    self.total_defense_text_box.text = Utilites.get_total_defense(
-      moral=self.item["Moral"], defense=self.item["Defense"]
-    )
 
   def text_box_change(self, **event_args):
     """This method is called when the text in this text box is edited"""
