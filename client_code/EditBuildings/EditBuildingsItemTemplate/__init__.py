125a882e3030b5f27ce408adfbdc490380a7e898
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2025-01-15 13:22:56 +0000

    Created EditBuildings.EditBuildingsItemTemplate; Edited EditBuildings; Deleted EditBuildings.ItemTemplate1

--- client_code/EditBuildings/EditBuildingsItemTemplate/__init__.py
+++ client_code/EditBuildings/EditBuildingsItemTemplate/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import ItemTemplate1Template
+from ._anvil_designer import EditBuildingsItemTemplateTemplate
 from anvil import *
 import anvil.users
 import anvil.tables as tables
@@ -6,7 +6,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class ItemTemplate1(ItemTemplate1Template):
+class EditBuildingsItemTemplate(EditBuildingsItemTemplateTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
