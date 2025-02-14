b2cf03ac87d7de1622ca6590da82ca1e54ec86e4
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2025-02-14 12:05:32 +0000

    Edited settings and Item

--- client_code/Item/__init__.py
+++ client_code/Item/__init__.py
@@ -21,6 +21,9 @@ class Item(ItemTemplate):
     self.player = None
     self.frosthaven = app_tables.frosthaven.search()[0]
 
+    self.one_herb = False
+    self.two_herbs = False
+
     self.setup()
     
   def setup(self):
@@ -37,23 +40,44 @@ class Item(ItemTemplate):
     self.set_visible()
     if not self.player:
       return
+    self.sufficiant = True
+    self.buy_button.visible = True
+    self.check_if_sufficiant_founds()
+    if self.sufficiant:
+      pass
+    else:
+      return
+      alert("Not enough gold")
 
-  def get_available_resources(self):
-    pass
-
+  def check_price(self, text_box, price):
+    if text_box.text > price:
+      self.sufficiant = False
+      text_box.background = 'theme:Primary'
+      
+  def check_if_sufficiant_founds(self):
+    self.check_price(self.gold_text_box, self.player['Gold'])
+    self.check_price(self.lumber_text_box, self.player['Lumber'])
+    self.check_price(self.metal_text_box, self.player['Metal'])
+    self.check_price(self.hide_text_box, self.player['Hide'])
+    
   def add_item_to_price(self, price_item):
-    self.set_price(price_item['Gold'], self.gold_image, self.gold_text_box)
-    self.set_price(price_item['Lumber'], self.lumber_image, self.lumber_text_box)
-    self.set_price(price_item['Metal'], self.metal_image, self.metal_text_box)
-    self.set_price(price_item['Hide'], self.hide_image, self.hide_text_box)
-    self.set_price(price_item['Arrowvine'], self.arrowvine_image, self.arrowvine_text_box)
-    self.set_price(price_item['Axenut'], self.axenut_image, self.axenut_text_box)
-    self.set_price(price_item['Corpsecap'], self.corpsecap_image, self.corpsecap_text_box)
-    self.set_price(price_item['Flamefruit'], self.flamefruit_image, self.flamefruit_text_box)
-    self.set_price(price_item['Rockroot'], self.rockroot_image, self.rockroot_text_box)
-    self.set_price(price_item['Snowthistle'], self.snowthistle_image, self.snwothistle_text_box)
+    self.set_price(price_item['Gold'], self.gold_text_box)
+    self.set_price(price_item['Lumber'], self.lumber_text_box)
+    self.set_price(price_item['Metal'], self.metal_text_box)
+    self.set_price(price_item['Hide'], self.hide_text_box)
+    self.set_price(price_item['Arrowvine'], self.arrowvine_text_box)
+    self.set_price(price_item['Axenut'], self.axenut_text_box)
+    self.set_price(price_item['Corpsecap'], self.corpsecap_text_box)
+    self.set_price(price_item['Flamefruit'], self.flamefruit_text_box)
+    self.set_price(price_item['Rockroot'], self.rockroot_text_box)
+    self.set_price(price_item['Snowthistle'], self.snwothistle_text_box)
+    if price_item['1Herb']:
+      self.one_herb = price_item['1Herb']
+    if price_item['2Herbs']:
+      self.two_herbs = price_item['2Herbs']
+    
 
-  def set_price(self, price, image, text_box):
+  def set_price(self, price, text_box):
     if price > 0:
       text_box.text += price
 
@@ -110,6 +134,12 @@ class Item(ItemTemplate):
     self.set_image_visible_if_source(self.item_cost_image_1)
     self.set_image_visible_if_source(self.item_cost_image_2)
     self.set_image_visible_if_source(self.item_cost_image_3)
+    if self.one_herb:
+      self.any_1_label.visible = True
+      self.any_1_drop_down.visible = True
+    if self.two_herbs:
+      self.any_2_label.visible = True
+      self.any_2_drop_down.visible = True
 
   def set_not_visible(self):
     self.gold_image.visible = False
@@ -135,6 +165,23 @@ class Item(ItemTemplate):
     self.item_cost_image_1.visible = False
     self.item_cost_image_2.visible = False
     self.item_cost_image_3.visible = False
+    self.any_1_label.visible = False
+    self.any_2_label.visible = False
+    self.any_1_drop_down.visible = False
+    self.any_2_drop_down.visible = False
+    self.buy_button.visible = False
+
+  def reset_border(self):
+    self.gold_text_box.background = ''
+    self.lumber_text_box.background = ''
+    self.metal_text_box.background = ''
+    self.hide_text_box.background = ''
+    self.arrowvine_text_box.background = ''
+    self.axenut_text_box.background = ''
+    self.corpsecap_text_box.background = ''
+    self.flamefruit_text_box.background = ''
+    self.rockroot_text_box.background = ''
+    self.snwothistle_text_box.background = ''
 
   def reset(self):
     self.set_not_visible()
@@ -148,9 +195,16 @@ class Item(ItemTemplate):
     self.flamefruit_text_box.text = 0
     self.rockroot_text_box.text = 0
     self.snwothistle_text_box.text = 0
+    
+    self.reset_border()
+    
     self.item_cost_image_1.source = None
     self.item_cost_image_2.source = None
     self.item_cost_image_3.source = None
+    self.one_herb = False
+    self.two_herbs = False
+    self.any_1_drop_down.selected_value = ''
+    self.any_2_drop_down.selected_value = ''
 
   def character_drop_down_change(self, **event_args):
     """This method is called when an item is selected"""
