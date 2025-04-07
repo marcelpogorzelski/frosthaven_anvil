60d1e00462ed17bcc2192c15048ca669f829722e
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2025-04-07 12:43:02 +0000

    Edited Login

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -13,9 +13,12 @@ class Login(LoginTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    #print(anvil.server.cookies)
+    if anvil.users.get_user(allow_remembered=True):
+      open_form(Main(start_form=Frosthaven()))
 
   def login_button_click(self, **event_args):
     anvil.users.login_with_form()
-    if anvil.users.get_user():
+    if anvil.users.get_user(allow_remembered=True):
       open_form(Main(start_form=Frosthaven()))
 
