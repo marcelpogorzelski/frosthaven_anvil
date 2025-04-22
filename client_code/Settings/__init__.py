3897761e3642e01b6548dcba29da39e6a0f68f86
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2025-04-22 04:51:45 +0000

    Edited settings and Settings

--- client_code/Settings/__init__.py
+++ client_code/Settings/__init__.py
@@ -18,7 +18,6 @@ class Settings(SettingsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    print(anvil.users.get_user())
 
     # Any code you write here will run before the form opens.
   def export_button_click(self, **event_args):
@@ -27,32 +26,12 @@ class Settings(SettingsTemplate):
 
   def import_file_loader_change(self, file, **event_args):
     """This method is called when a new file is loaded into this FileLoader"""
-    item_prices = json.loads(file.get_bytes())
+    file_data = json.loads(file.get_bytes())
 
-    for item_price in item_prices:
-      item = app_tables.items.get(Number=item_price['Number'], Name=item_price['Name'])
-      if not item:
-        continue
-      item['Gold'] = parse_int_price(item_price['Gold'])
-      item['Lumber'] = parse_int_price(item_price['Lumber'])
-      item['Metal'] = parse_int_price(item_price['Metal'])
-      item['Hide'] = parse_int_price(item_price['Hide'])
-      item['Arrowvine'] = parse_int_price(item_price['Arrowvine'])
-      item['Axenut'] = parse_int_price(item_price['Axenut'])
-      item['Corpsecap'] = parse_int_price(item_price['Corpsecap'])
-      item['Rockroot'] = parse_int_price(item_price['Rockroot'])
-      item['Flamefruit'] = parse_int_price(item_price['Flamefruit'])
-      item['Snowthistle'] = parse_int_price(item_price['Snowthistle'])
-      item['1Herb'] = False
-      if item_price['1Herb'] == 1:
-        item['1Herb'] = True
-      item['2Herbs'] = False
-      if item_price['2Herbs'] == 1:
-        item['2Herbs'] = True
-      item['Items'] = item_price['Items']
-      item['TotalCount'] = parse_int_price(item_price['Count'])
-      item['AvailableCount'] = parse_int_price(item_price['Count'])
-      item.update()
+    for treasure in file_data:
+      app_tables.treasures.add_row(Number=treasure['Number'], Content=treasure['Treasure'], Looted=False)
+
+
 
   def change_password_button_click(self, **event_args):
     """This method is called when the button is clicked"""
