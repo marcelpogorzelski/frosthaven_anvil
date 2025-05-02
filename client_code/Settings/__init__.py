6164f5959754c5124fd0ea5230d37897112213f8
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2025-05-02 22:27:12 +0000

    Edited settings and Settings

--- client_code/Settings/__init__.py
+++ client_code/Settings/__init__.py
@@ -8,99 +8,10 @@ import json
 from anvil.tables import app_tables
 from .. import Utilites
 
-
-pets = [
-    {
-      'id': 1,
-      'name': "Piranha Pig",
-      'game': "fh",
-      'image': "pets/frosthaven/fh-01-piranha-pig.jpeg",
-      'imageBack': "pets/frosthaven/fh-01-piranha-pig-back.jpeg",
-    },
-    {
-      'id': 2,
-      'name': "Hound",
-      'game': "fh",
-      'image': "pets/frosthaven/fh-02-hound.jpeg",
-      'imageBack': "pets/frosthaven/fh-02-hound-back.jpeg",
-    },
-    {
-      'id': 3,
-      'name': "Spitting Drake",
-      'game': "fh",
-      'image': "pets/frosthaven/fh-03-spitting-drake.jpeg",
-      'imageBack': "pets/frosthaven/fh-03-spitting-drake-back.jpeg",
-    },
-    {
-      'id': 4,
-      'name': "Rending Drake",
-      'game': "fh",
-      'image': "pets/frosthaven/fh-04-rending-drake.jpeg",
-      'imageBack': "pets/frosthaven/fh-04-rending-drake-back.jpeg",
-    },
-    {
-      'id': 5,
-      'name': "Black Imp",
-      'game': "fh",
-      'image': "pets/frosthaven/fh-05-black-imp.jpeg",
-      'imageBack': "pets/frosthaven/fh-05-black-imp-back.jpeg",
-    },
-    {
-      'id': 6,
-      'name': "Forest Imp",
-      'game': "fh",
-      'image': "pets/frosthaven/fh-06-forest-imp.jpeg",
-      'imageBack': "pets/frosthaven/fh-06-forest-imp-back.jpeg",
-    },
-    {
-      'id': 7,
-      'name': "Snow Imp",
-      'game': "fh",
-      'image': "pets/frosthaven/fh-07-snow-imp.jpeg",
-      'imageBack': "pets/frosthaven/fh-07-snow-imp-back.jpeg",
-    },
-    {
-      'id': 8,
-      'name': "Ooze",
-      'game': "fh",
-      'image': "pets/frosthaven/fh-08-ooze.jpeg",
-      'imageBack': "pets/frosthaven/fh-08-ooze-back.jpeg",
-    },
-    {
-      'id': 9,
-      'name': "Ruined Machine",
-      'game': "fh",
-      'image': "pets/frosthaven/fh-09-ruined-machine.jpeg",
-      'imageBack': "pets/frosthaven/fh-09-ruined-machine-back.jpeg",
-    },
-    {
-      'id': 10,
-      'name': "Lightning Eel",
-      'game': "fh",
-      'image': "pets/frosthaven/fh-10-lightning-eel.jpeg",
-      'imageBack': "pets/frosthaven/fh-10-lightning-eel-back.jpeg",
-    },
-    {
-      'id': 11,
-      'name': "Heroics",
-      'game': "fh",
-      'image': "pets/frosthaven/fh-11-heroics.jpeg",
-      'imageBack': "pets/frosthaven/fh-11-heroics-back.jpeg",
-    },
-    {
-      'id': 12,
-      'name': "Brummix",
-      'game': "fh",
-      'image': "pets/frosthaven/fh-12-brummix.jpeg",
-      'imageBack': "pets/frosthaven/fh-12-brummix-back.jpeg",
-    },
-  ]
-
 class Settings(SettingsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
 
     self.check_items()
     
@@ -154,15 +65,27 @@ class Settings(SettingsTemplate):
     scenario = app_tables.scenarios.get(Name=name)
 
     scenario.update(Loot=loot)
-    
+
+  def parse_achievements(self, achievements):
+    exclude = list()
+
+    for achievement in achievements:
+      if achievement['id'] in exclude:
+        continue
+      id = achievement.get('id')
+      name = achievement.get('name')
+      group = achievement.get('group')
+      type = achievement.get('type')
+      upgrades = achievement.get('upgrades', [])
+      for upgrade in upgrades:
+        exclude.append(upgrade)
+      upgrades = len(upgrades)
+      app_tables.achievements.add_row(Id=id, Name=name, Group=group, Type=type, Upgrades=upgrades)
     
   def import_file_loader_change(self, file, **event_args):
     """This method is called when a new file is loaded into this FileLoader"""
     file_data = json.loads(file.get_bytes())
-
-    for scenario_id, scenario_data in file_data.items():
-      break
-      #self.parse_scenario_errata(scenario_id, scenario_data)
+    self.parse_achievements(file_data)
 
   def change_password_button_click(self, **event_args):
     """This method is called when the button is clicked"""
