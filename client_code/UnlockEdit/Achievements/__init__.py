3e2f5ed1b7ddf01703f6446f5feb04e9f602a1af
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2025-05-03 06:10:37 +0000

    Created UnlockEdit.Achievements; Edited settings, Settings and UnlockEdit

---
+++ client_code/UnlockEdit/Achievements/__init__.py
@@ -0,0 +1,46 @@
+from ._anvil_designer import AchievementsTemplate
+from anvil import *
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class Achievements(AchievementsTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    self.achievement_drop_down.items = [(achievement['Name'], achievement) for achievement in app_tables.achievements.search()]
+    self.item = self.achievement_drop_down.selected_value
+
+  def get_image_source(self):
+    if not self.item['CurrentLevel']:
+      self.achievement_image.source = f"https://raw.githubusercontent.com/teamducro/gloomhaven-storyline/refs/heads/master/resources/img/achievements/{self.item['Id']}.png"
+    number = ''
+    if not self.item['CurrentLevel'] > 0:
+      pass
+      
+
+  def show_levels(self):
+    if not self.item['Available']:
+      return False
+    if self.item['Upgrades']:
+      return True
+    return False
+
+  def change_group(self):
+    for achievement in app_tables.achievements.search(Group=self.item['Group']):
+      if achievement == self.item:
+        continue
+      achievement['Available'] = False
+
+  def available_check_box_change(self, **event_args):
+    if self.item['Group'] and self.available_check_box.checked:
+      self.change_group()
+    self.item['Available'] = self.available_check_box.checked
+    self.refresh_data_bindings()
+
+  def achievement_drop_down_change(self, **event_args):
+    self.item = self.achievement_drop_down.selected_value
+    self.refresh_data_bindings()
--- client_code/UnlockEdit/__init__.py
+++ client_code/UnlockEdit/__init__.py
@@ -12,6 +12,7 @@ from .Items import Items
 from .Buildings import Buildings
 from .Classes import Classes
 from .Pets import Pets
+from .Achievements import Achievements
 
 class UnlockEdit(UnlockEditTemplate):
   def __init__(self, **properties):
@@ -21,16 +22,18 @@ class UnlockEdit(UnlockEditTemplate):
     self.buildings_form = Buildings()
     self.classes_from = Classes()
     self.pets_form = Pets()
+    self.achievements_form = Achievements()
     
     self.init_components(**properties)
 
     self.change_form(self.scenarios_form)
     
-    self.flow_panel_2.add_component(self.scenarios_form)
-    self.flow_panel_2.add_component(self.items_form)
-    self.flow_panel_2.add_component(self.buildings_form)
-    self.flow_panel_2.add_component(self.classes_from)
-    self.flow_panel_2.add_component(self.pets_form)
+    self.form_flow_panel.add_component(self.scenarios_form)
+    self.form_flow_panel.add_component(self.items_form)
+    self.form_flow_panel.add_component(self.buildings_form)
+    self.form_flow_panel.add_component(self.classes_from)
+    self.form_flow_panel.add_component(self.pets_form)
+    self.form_flow_panel.add_component(self.achievements_form)
     
 
   def reset_forms(self):
@@ -39,6 +42,7 @@ class UnlockEdit(UnlockEditTemplate):
     self.buildings_form.visible = False
     self.classes_from.visible = False
     self.pets_form.visible = False
+    self.achievements_form.visible = False
 
   def change_form(self, form):
     self.reset_forms()
