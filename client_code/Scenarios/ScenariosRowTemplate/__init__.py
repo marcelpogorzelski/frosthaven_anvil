7374c25eccf671768918e098eb4fe8363fb7dd63
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2025-05-09 12:37:07 +0000

    Edited settings, Scenarios.ScenariosRowTemplate, Scenarios and Utilites

--- client_code/Scenarios/ScenariosRowTemplate/__init__.py
+++ client_code/Scenarios/ScenariosRowTemplate/__init__.py
@@ -14,11 +14,14 @@ class ScenariosRowTemplate(ScenariosRowTemplateTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.check_name_link_availability()
     self.status_drop_down.items = Utilites.get_scenario_statuses(self.item)
 
-  def check_name_link(self):
-    if self.item['Status'] == Utilites:
-      pass
+  def check_name_link_availability(self):
+    if self.item['Status'] not in Utilites.SCENARIO_NOT_AVAILABLE:
+      self.name_button.enabled = True
+      return
+    self.name_button.enabled = False
     
   def foregroud_color(self, hex):
     red, green, blue = tuple(int(hex[i:i+2], 16) for i in (1, 3, 5))
@@ -29,5 +32,9 @@ class ScenariosRowTemplate(ScenariosRowTemplateTemplate):
     else:
       return "#000000"
 
-  def name_link_click(self, **event_args):
+  def name_button_click(self, **event_args):
     navigation.go_to_scenario(self.item)
+
+  def status_drop_down_change(self, **event_args):
+    self.item['Status'] = self.status_drop_down.selected_value
+    self.check_name_link_availability()
