c00fc5d5565640bc897907e262cbec64ddfbceae
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2025-05-23 10:09:59 +0000

    Edited OutpostPhase.Content.BuildingOperations.MiningLoggingHunting

--- client_code/OutpostPhase/Content/BuildingOperations/MiningLoggingHunting/__init__.py
+++ client_code/OutpostPhase/Content/BuildingOperations/MiningLoggingHunting/__init__.py
@@ -8,6 +8,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 from itertools import cycle
+from functools import reduce
 
 class MiningLoggingHunting(MiningLoggingHuntingTemplate):
   def __init__(self, gamestate, finish_phase_tag, **properties):
@@ -21,7 +22,6 @@ class MiningLoggingHunting(MiningLoggingHuntingTemplate):
     if self.finished:
       self.disable_phase()
 
-    
     self.resource_repeating_panel.items = [
       {'Image': '_/theme/resource_images/fh-lumber-bw-icon.png', 'BuildingNumber': 17, 'Resource': 'Lumber'},
       {'Image': '_/theme/resource_images/fh-metal-bw-icon.png', 'BuildingNumber': 5, 'Resource': 'Metal'},
@@ -54,18 +54,18 @@ class MiningLoggingHunting(MiningLoggingHuntingTemplate):
       {'Character': character, 'Cost': 0, 'TotalGold': character['Gold']} for character in self.characters
     ]
 
-    total = self.total_gold
-    while total > 0:
-      temp_total = total
-      for character_item in self.character_items:
-        if character_item['TotalGold'] == 0:
-          continue
-        character_item['Cost'] += 1
-        character_item['TotalGold'] -= 1
-        total -= 1
-        if total == 0:
-          break
-      if temp_total == total:
+    character_total = reduce(lambda sum, character: sum + character['TotalGold'], self.character_items, 0)
+    character_total = int(character_total / 2) * 2
+    
+    total = min(self.total_gold, character_total)
+    
+    for character_item in cycle(self.character_items):
+      if character_item['TotalGold'] <= 0:
+        continue
+      character_item['Cost'] += 1
+      character_item['TotalGold'] -= 1
+      total -= 1
+      if total <= 0:
         break
 
     self.characters_repeating_panel.items = sorted(self.character_items, key=lambda x: x['Character']['Player'])
