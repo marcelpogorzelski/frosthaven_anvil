792559ecd05bd23998cb1f9497b69a56d6bd1af7
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2025-06-03 10:04:58 +0000

    Edited UnlockEdit.GloomhavenItems

--- client_code/UnlockEdit/GloomhavenItems/__init__.py
+++ client_code/UnlockEdit/GloomhavenItems/__init__.py
@@ -12,24 +12,24 @@ class GloomhavenItems(GloomhavenItemsTemplate):
   def __init__(self, **properties):
 
     self.card_selected = False
-    self.image_file = None
+    self.none_item = {
+      'image': None,
+      'cost': None
+    }
+    
+    self.item = self.none_item
 
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-    
 
     self.gloomhaven_items = json.loads(app_tables.files.get(path='other_items.json')['file'].get_bytes())['gh']
 
 
   def number_text_box_pressed_enter(self, **event_args):
     item_number = self.number_text_box.text
-    self.item = next(filter(lambda item: item['id'] == item_number, self.gloomhaven_items), None)
+    self.item = next(filter(lambda item: item['id'] == item_number, self.gloomhaven_items), self.none_item)
 
-    if not self.item:
-      self.hide_item()
-    else:
-      self.show_item()
+    self.show_item()
 
   def hide_item(self):
     self.item = None
@@ -39,12 +39,17 @@ class GloomhavenItems(GloomhavenItemsTemplate):
     self.refresh_data_bindings()
 
   def show_item(self):
-    self.card_selected = True
-    self.image_file = self.get_image(self.item['image'])
+    if self.item is self.none_item:
+      self.card_selected = False
+      self.number_text_box.text = None
+    else:
+      self.card_selected = True
     self.refresh_data_bindings()
 
-  def get_image(self, path):
-    url = f"https://raw.githubusercontent.com/cmlenius/gloomhaven-card-browser/images/images/{path}"
+  def get_image(self):
+    if self.item is self.none_item:
+      return None
+    url = f"https://raw.githubusercontent.com/cmlenius/gloomhaven-card-browser/images/images/{self.item['image']}"
     return URLMedia(url)
     
 
