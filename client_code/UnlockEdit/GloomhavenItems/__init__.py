692330983764b7069d05245966ac8d5458605665
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2025-06-04 08:48:11 +0000

    Edited UnlockEdit.GloomhavenItems

--- client_code/UnlockEdit/GloomhavenItems/__init__.py
+++ client_code/UnlockEdit/GloomhavenItems/__init__.py
@@ -41,8 +41,21 @@ class GloomhavenItems(GloomhavenItemsTemplate):
 
     if not self.item:
       self.hide_item()
-    else:
-      self.show_item()
+      return
+
+    self.item['number'] = f"G{self.item['id']}"
+    if self.already_in_store():
+      self.hide_item()
+      return
+    self.show_item()
+
+  def already_in_store(self):
+    item = app_tables.items.get(Number=self.item['number'])
+    if not item:
+      return False
+
+    Notification(F"Gloomhaven Item {item['Name']} is already in store", timeout=6).show()
+    return True
 
   def hide_item(self):
     self.number_text_box.text = None
@@ -53,12 +66,20 @@ class GloomhavenItems(GloomhavenItemsTemplate):
     self.card_selected = True
     if 'image_file' not in self.item:
       self.item['image_file'] = self.get_image()
+
+    if 'title_name' not in self.item:
+      self.item['title_name'] = self.item['name'].title()
+
+    if 'count' not in self.item:
+      self.item['count'] = 2
     if 'usage' not in self.item:
       self.item['usage'] = 'Passive'
       if 'spent' in self.item:
         self.item['usage'] = 'Spent'
       if 'consumed' in self.item:
         self.item['usage'] = 'Lost'
+
+    self.item['type'] = SLOT_CONVERSION[self.item['slot']]
         
     self.refresh_data_bindings()
 
@@ -68,4 +89,33 @@ class GloomhavenItems(GloomhavenItemsTemplate):
     
 
   def add_button_click(self, **event_args):
-    pass
+    if not confirm(f"Add Gloomhaven Item: {self.item['title_name']}"):
+      return
+
+    new_item = app_tables.items.add_row(
+      Number=self.item['number'],
+      Name=self.item['title_name'],
+      Card=self.item['image_file'],
+      Type=self.item['type'],
+      Usage=self.item['usage'],
+      HasGoldCost=True,
+      Available=True,
+      Gold=self.item['cost'],
+      Lumber=0,
+      Metal=0,
+      Hide=0,
+      Arrowvine=0,
+      Axenut=0,
+      Corpsecap=0,
+      Flamefruit=0,
+      Rockroot=0,
+      Snowthistle=0,
+      TotalCount=self.item['count'],
+      AvailableCount=self.item['count'],
+      Destilled=False
+    )
+    
+    new_item['1Herb'] = False
+    new_item['2Herbs'] = False
+
+    Notification(F"Gloomhaven Item {self.item['title_name']} added",timeout=6).show()
