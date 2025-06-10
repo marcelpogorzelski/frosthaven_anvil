87f61e7a0a8a5fb7a9193ed7521f64070e7d3d08
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2025-06-10 05:05:40 +0000

    Created Character.CharacterPerks.PerksItemTemplate.TwoChecks and Character.CharacterPerks.PerksItemTemplate.ThreeChecks; Edited Character.CharacterPerks.PerksItemTemplate.PerkIcon and Character.CharacterPerks.PerksItemTemplate; Deleted Character.CharacterPerks.ThreeChecks and Character.CharacterPerks.TwoChecks

--- client_code/Character/CharacterPerks/PerksItemTemplate/PerkIcon/__init__.py
+++ client_code/Character/CharacterPerks/PerksItemTemplate/PerkIcon/__init__.py
@@ -12,6 +12,8 @@ class PerkIcon(PerkIconTemplate):
   def __init__(self, perk_file, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    #height = 22
+    #if perk_file == 'heal.png':
+      height = 20
     self.image_1.source = f"_/theme/perk_icons/{perk_file}"
-    print(self.image_1.spacing_above)
+    self.image_1.height = height
--- client_code/Character/CharacterPerks/PerksItemTemplate/__init__.py
+++ client_code/Character/CharacterPerks/PerksItemTemplate/__init__.py
@@ -7,6 +7,8 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 from .PerkIcon import PerkIcon
+from .TwoChecks import TwoChecks
+from .ThreeChecks import ThreeChecks
 
 import re
 
@@ -19,8 +21,12 @@ class PerksItemTemplate(PerksItemTemplateTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    slots = extract_slots(self.item['desc'])
-    self.perk_rich_text.content = self.item['desc']
 
+    slots = extract_slots(self.item['desc'])
+    self.perk_rich_text.content = "{check1}" + "{check2}" + "{check3}" + self.item['desc']
+    #self.perk_rich_text.add_component(ThreeChecks(), slot='drop')
+    self.perk_rich_text.add_component(CheckBox(text=None), slot='check1')
+    self.perk_rich_text.add_component(CheckBox(text=None), slot='check2')
+    self.perk_rich_text.add_component(CheckBox(text=None), slot='check3')
     for slot in slots:
       self.perk_rich_text.add_component(PerkIcon(slot), slot=slot)
