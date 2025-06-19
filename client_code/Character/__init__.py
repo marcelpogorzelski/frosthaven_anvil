c97e918558ed0d4b0d9b50eb69ba9fee04f18010
marcelpogorzelski@gmail.com <marcelpogorzelski@gmail.com>
2025-06-19 21:59:50 +0000

    Edited Character

--- client_code/Character/__init__.py
+++ client_code/Character/__init__.py
@@ -82,7 +82,7 @@ class Character(CharacterTemplate):
 
   def setup_label(self):
     name_label = Label(text=self.player_name, role='title', align='center')
-    name_card = ColumnPanel(role='elevated-card', wrap_on='naver')
+    name_card = ColumnPanel(role='elevated-card', wrap_on='naver', spacing_above='none', spacing_below='none')
     name_card.add_component(name_label)
     self.label_flow_panel.add_component(name_card, width=900)
 
